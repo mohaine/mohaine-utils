fd6561770cf428852380ebdb92adbdf116c7b8da
Michael Graessle <mike@graessle.net>
2014-06-06 07:34:12 -0500

    Cleanups

--- src/main/java/com/mohaine/db/PrintableStatementProxy.java
+++ src/main/java/com/mohaine/db/PrintableStatementProxy.java
@@ -7,8 +7,6 @@ import java.sql.SQLWarning;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class PrintableStatementProxy implements Statement {
 	private List<String> batchedSql = null;
